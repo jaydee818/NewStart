cdeea07e329baa01bc991d292d0008244f9f4e61
jaydee818 <jaydee818@gmail.com>
2014-04-14 14:19:37 -0700

    added Code Academy practice .js

---
+++ CodeAcademy_practice.js
@@ -0,0 +1,9 @@
+// practicing stuff with arrays and loops
+
+
+var text = "Hi, how are you doing? \
+My name is JD. \
+What is your name? \
+Like I said my name is JD";
+var myName = "JD";
+var hits
